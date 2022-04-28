c02cbc17bc3f01c0550b38c195f4419fb0b936f7
AndrewOrozco <andreworozco62@gmail.com>
2022-04-28 15:15:41 -0500

    git add -p

--- QuintoPunto.py
+++ QuintoPunto.py
@@ -1,10 +1,11 @@
 # Programa que me calcula numero mayor de N Entradas
-numeros = []
-for i in range(3):
-  numero =input("Introduce el número #{}: ".format(i + 1))
-  numeros.append(numero)
-mayor = numeros[0]
-for numero in numeros:
-    if numero > mayor:
-        mayor = numero
-print("El numero Mayor es :", mayor)
+a = float(input(" Ingrese el nuemro {} ".format(1)))
+b = float(input(" Ingrese el nuemro {} ".format(2)))
+c = float(input(" Ingrese el nuemro {} ".format(3)))
+
+if b<a>c:
+  print("El nuemro mayor: ", a)
+elif a<b>c:
+  print("El nuemro mayor: ", b)
+elif a<c>b:
+  print("El nuemro mayor: ", c)
